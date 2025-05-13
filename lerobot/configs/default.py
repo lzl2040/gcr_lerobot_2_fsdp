beb2f695b2d0f91b86527ea67b7418d77d7eaf3b
lzl2040 <2040171586@qq.com>
2025-05-13 06:08:00 +0000

    fix wandb_id

--- lerobot/configs/default.py
+++ lerobot/configs/default.py
@@ -51,7 +51,7 @@ class WandBConfig:
     project: str = "lerobot"
     entity: str | None = None
     notes: str | None = None
-    id: str = "7258056839.59298-05824d03-26a7-488c-abda-e9b82731f1c5"
+    id: str = "7253064995.07085-3c68a25c-1635-4716-83e2-c8934800636e"
 
 
 @dataclass
